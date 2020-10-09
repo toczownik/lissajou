fd306a53c212e4d4a1afd28105f97f0890247df1
Jan Szataniak <jan.szataniak@gmail.com>
2020-10-09 19:01:25 +0200

    Implemented Lissajous formula and simple graph display.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,22 +1,42 @@
 #include "mainwindow.h"
 #include <QGridLayout>
+#include <QtMath>
 
 MainWindow::MainWindow() {
+    createSeries(1000, 2, 5, 1, 3, M_PI_2);
+    prepareChart();
+
     auto *widget = new QWidget;
     setCentralWidget(widget);
 
-    auto *topFiller = new QWidget;
-    topFiller->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-
-    auto *bottomFiller = new QWidget;
-    bottomFiller->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
     auto *layout = new QVBoxLayout;
     layout->setContentsMargins(5, 5, 5, 5);
-    layout->addWidget(topFiller);
-    layout->addWidget(bottomFiller);
+    layout->addWidget(chartView);
     widget->setLayout(layout);
 
-    setMinimumSize(160, 160);
-    resize(480, 320);
+    setMinimumSize(640, 480);
+}
+
+MainWindow::~MainWindow() {
+    delete series;
+    delete chart;
+    delete chartView;
+    delete[] sliders;
+    delete sliders;
+}
+
+void MainWindow::createSeries(int size, qreal A, qreal a, qreal B, qreal b, qreal f) {
+    series = new QLineSeries;
+    for (int i = 0; i < size; ++i) {
+        series->append(A * qSin(a * i + f), B * qCos(b * i));
+    }
+}
+
+void MainWindow::prepareChart() {
+    chart = new QChart;
+    chart->legend()->hide();
+    chart->addSeries(series);
+    chartView = new QChartView(chart);
+    chartView->setRenderHint(QPainter::Antialiasing);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,39 @@
 #define LISSAJOU_MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtGlobal>
+#include <QChart>
+#include <QChartView>
+#include <QLineSeries>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QSlider>
+
+enum variables {
+    A = 0,
+    a = 1,
+    B = 2,
+    b = 3,
+    f = 4
+};
+
+using namespace QtCharts;
 
 class MainWindow: public QMainWindow {
     Q_OBJECT
 public:
     MainWindow();
+    ~MainWindow() override;
+
+private:
+    QLineSeries *series;
+    QChart *chart;
+    QChartView *chartView;
+
+    QSlider **sliders;
+
+    void createSeries(int size, qreal A, qreal a, qreal B, qreal b, qreal f);
+    void prepareChart();
 };
+
 #endif //LISSAJOU_MAINWINDOW_H
